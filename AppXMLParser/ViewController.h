32e94481ec8f041d16e2f3c4f912cf66c25c1e01
Duc Hieu Pham <hieup@outlook.com>
2013-08-27 21:56:45 -0500

    Initial Commit

---
+++ AppXMLParser/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AppXMLParser
+//
+//  Created by Hieu on 8/27/13.
+//  Copyright (c) 2013 Hieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AppXMLParser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppXMLParser
+//
+//  Created by Hieu on 8/27/13.
+//  Copyright (c) 2013 Hieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
